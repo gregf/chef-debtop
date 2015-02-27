9e41e43ed68f6a66487a5aaf08cf043d24ed208f
Greg Fitzgerald <greg@gregf.org>
2015-02-27 13:09:21 -0500

    Add packages cookbook

---
+++ cookbooks/packages/metadata.rb
@@ -0,0 +1,8 @@
+name             'packages'
+maintainer       'Greg Fitzgerald'
+maintainer_email 'greg@gregf.org'
+license          'apache2'
+description      'Installs/Configures packages'
+long_description 'Installs/Configures packages'
+version          '0.1.0'
+
