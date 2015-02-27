dfa222c703bfce6cfce65dbd7cfbed7732053cc6
Greg Fitzgerald <greg@gregf.org>
2015-02-27 13:09:02 -0500

    Add mail cookbook

---
+++ cookbooks/mail/README.md
@@ -0,0 +1,4 @@
+# mail
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/mail/metadata.rb
@@ -0,0 +1,8 @@
+name             'mail'
+maintainer       'Greg Fitzgerald'
+maintainer_email 'greg@gregf.org'
+license          'apache2'
+description      'Installs/Configures mail'
+long_description 'Installs/Configures mail'
+version          '0.1.0'
+
