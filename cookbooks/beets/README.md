cb06f752cfa94dc530e6da3cbfae5e257b182416
Greg Fitzgerald <greg@gregf.org>
2015-02-28 18:32:19 -0500

    Add cookbook for beets

---
+++ cookbooks/beets/README.md
@@ -0,0 +1,4 @@
+# beets
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/beets/metadata.rb
@@ -0,0 +1,8 @@
+name             'beets'
+maintainer       'Greg Fitzgerald'
+maintainer_email 'greg@gregf.org'
+license          'apache2'
+description      'Installs/Configures beets'
+long_description 'Installs/Configures beets'
+version          '0.1.0'
+
