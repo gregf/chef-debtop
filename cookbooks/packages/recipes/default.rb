fefd93a15d9b8d0b87d328c3e325f3a2dd51a877
Greg Fitzgerald <greg@gregf.org>
2015-02-28 15:00:01 -0500

    Add docker to packages list

--- cookbooks/packages/recipes/default.rb
+++ cookbooks/packages/recipes/default.rb
@@ -18,7 +18,7 @@
 
 pkgs = %W(vim-gtk htop zsh pulseaudio pavucontrol python-pip weechat feh openntpd
           most rsync deluge filezilla bitlbee mpv vlc lxappearance cups splix
-      	  gtk2-engines taskwarrior)
+          gtk2-engines taskwarrior docker.io)
 
 pkgs.each do |pkg|
   package pkg
